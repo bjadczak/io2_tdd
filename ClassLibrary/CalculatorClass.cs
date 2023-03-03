dfddf9558a93b52dac5ab17aa186e9367f93d291
jadczak bartosz <73365815+bjadczak@users.noreply.github.com>
2023-03-03 10:24:17 +0100

    Add test for calculator

---
+++ ClassLibrary/CalculatorClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Calculator
+    {
+        public int Add(string input)
+        {
+            if(String.IsNullOrEmpty(input))
+                return 0;
+            if(int.TryParse(input, out int result) && result >= 0) 
+                return result <= 1000 ? result : 0;
+            else if(result < 0) throw new ArgumentOutOfRangeException("NEG");
+            if(input.Contains(",") || input.Contains("\n"))
+            {
+                var numbers = input.Split(',', '\n');
+
+                if(numbers.Length >=2 && numbers.Length <= 3)
+                {
+                    int sum = 0;
+                    foreach(var number in numbers)
+                    {
+                        if (int.TryParse(number, out int num))
+                            sum += num <= 1000 ? num : 0;
+                        else
+                            return 0;
+                    }
+                    return sum;
+                }
+
+            }
+            return 0;
+        }
+    }
+}
--- TestProject/CalcTests.cs
+++ TestProject/CalcTests.cs
@@ -4,13 +4,17 @@ namespace TestProject
 {
     public class CalcTests
     {
+        private readonly Calculator _calculator;
+
+        public CalcTests()
+        {
+            _calculator = new Calculator();
+        }
 
         [Fact]
         public void WhenEmptyStringProvided_ReturnZero()
         {
-            var calc = new Calculator();
-
-            Assert.Equal(0, calc.Add(""));
+            Assert.Equal(0, _calculator.Add(""));
 
         }
         [Theory]
@@ -23,16 +27,69 @@ namespace TestProject
         [InlineData("7", 7)]
         [InlineData("8", 8)]
         [InlineData("9", 9)]
-        [InlineData("-1", -1)]
         [InlineData("0", 0)]
-        [InlineData("23423423", 23423423)]
-        [InlineData("-23423423", -23423423)]
         public void WhenOneNumberProvided_ReturnNumber(string number, int expected)
         {
-            var calc = new Calculator();
 
-            Assert.Equal(expected, calc.Add(number));
+            Assert.Equal(expected, _calculator.Add(number));
 
+        }
+        [Theory]
+        [InlineData("1,1", 2)]
+        [InlineData("1,2", 3)]
+        [InlineData("2,1", 3)]
+        [InlineData("200,1", 201)]
+        [InlineData("0,0", 0)]
+        public void WhenTwoNumbersSeparetaedByComaPrivided_ReturnSum(string number, int expected)
+        {
+
+            Assert.Equal(expected, _calculator.Add(number));
+
+        }
+        [Theory]
+        [InlineData("1\n1", 2)]
+        [InlineData("1\n2", 3)]
+        [InlineData("2\n1", 3)]
+        [InlineData("200\n1", 201)]
+        [InlineData("0\n0", 0)]
+        public void WhenTwoNumbersSeparetaedByNewLinePrivided_ReturnSum(string number, int expected)
+        {
+
+            Assert.Equal(expected, _calculator.Add(number));
+
+        }
+        [Theory]
+        [InlineData("1\n1,3", 5)]
+        [InlineData("1\n2,1", 4)]
+        [InlineData("2,1\n0", 3)]
+        [InlineData("200\n1,100", 301)]
+        [InlineData("200,1,100", 301)]
+        [InlineData("200,1\n100", 301)]
+        [InlineData("200\n1\n100", 301)]
+        [InlineData("0\n0\n0", 0)]
+        [InlineData("0,0,0", 0)]
+        [InlineData("0,0\n0", 0)]
+        [InlineData("0\n0,0", 0)]
+        public void WhenThreeNumbersSeparetaedByNewLineOrComaPrivided_ReturnSum(string number, int expected)
+        {
+
+            Assert.Equal(expected, _calculator.Add(number));
+
+        }
+        [Theory]
+        [InlineData("-1")]
+        public void WhenNegativeNumberProvided_ExpectException(string number)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.Add(number));
+        }
+        [Theory]
+        [InlineData("23423423", 0)]
+        [InlineData("200\n1\n1001", 201)]
+        [InlineData("200\n1\n1001,1", 202)]
+        [InlineData("1\n1001", 1)]
+        public void WhenNumberGreaterThan1000_Ignore(string number, int expected)
+        {
+            Assert.Equal(expected, _calculator.Add(number));
         }
     }
 }
